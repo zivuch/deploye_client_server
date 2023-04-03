692486c001e9a4cf5c7f97d5aaab57c7c2e49e0c
zivuch <zivuch@gmail.com>
2023-04-03 17:59:01 +0300

    client server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors')
+const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -10,4 +11,13 @@ app.get("/api/:name", (req, res) => {
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server listening on ${process.env.PORT || 3001}`);
+});
+
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
