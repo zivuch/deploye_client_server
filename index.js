53e369eb7ee737ae3a22f494b2d92d635a072a6c
zivuch <zivuch@gmail.com>
2023-06-22 08:12:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/:name", (req, res) => {
-  res.json({ message: `Hello from ${req.params.name} server!` });
+  res.json({ message: `Hello ${req.params.name}, from server!` });
 });
 
 app.listen(process.env.PORT || 3001, () => {
