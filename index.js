9ac7d17eda8a7006e8650e588b6d45e501232947
zivuch <zivuch@gmail.com>
2023-09-21 13:56:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.get("/api/:name", (req, res) => {
 
 // Have Node serve the files for our built React app
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 
