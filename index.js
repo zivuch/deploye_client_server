5a2a8f9646d50814b45b94edf5a0e5654b1ff294
zivuch <zivuch@gmail.com>
2025-01-14 17:16:50 +0200

    first commit

--- index.js
+++ index.js
@@ -15,13 +15,9 @@ app.get("/api/:name", (req, res) => {
 
 // Have Node serve the files for our built React app
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client1/dist")));
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client1/dist", "index.html"));
 });
-
-
-
-
